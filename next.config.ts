43c51716b02f93b39fde890013e8cc22d5d74d62
Yurong <18810399670@163.com>
2026-03-25 14:22:52 +0800

    fix: next.config.ts 添加 192.168.0.101 到白名单

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.0.100', 'localhost'],
+  allowedDevOrigins: ['192.168.0.100', '192.168.0.101', 'localhost'],
 };
 
 export default nextConfig;
